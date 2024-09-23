d178e21f3650f4a6630819624f3138de5d1046a7
tm-fuad05 <tm.fuad05@gmail.com>
2024-09-24 01:17:42 +0600

    transection part complete

--- JS/donation-history.js
+++ JS/donation-history.js
@@ -1,1 +1,36 @@
-console.log("Berelam");
+document.getElementById("history-btn").addEventListener("click", function () {
+  const historyBtn = document.getElementById("history-btn");
+
+  historyBtn.classList.remove("border", "border-stroke", "text-para");
+  historyBtn.classList.add("bg-primary", "text-black");
+
+  const donationBtn = document.getElementById("donation-btn");
+
+  donationBtn.classList.remove("bg-primary");
+  donationBtn.classList.add("text-para", "border", "border-stroke");
+
+  const donationPage = document.getElementById("donation-page");
+
+  donationPage.classList.add("hidden", true);
+
+  const historyFigure = document.getElementById("history-page");
+  historyFigure.classList.remove("hidden");
+});
+document.getElementById("donation-btn").addEventListener("click", function () {
+  const historyBtn = document.getElementById("history-btn");
+
+  historyBtn.classList.add("border", "border-stroke", "text-para");
+  historyBtn.classList.remove("bg-primary", "text-black");
+
+  const donationBtn = document.getElementById("donation-btn");
+
+  donationBtn.classList.add("bg-primary");
+  donationBtn.classList.remove("text-para", "border", "border-stroke");
+
+  const donationPage = document.getElementById("donation-page");
+
+  donationPage.classList.remove("hidden", true);
+
+  const historyFigure = document.getElementById("history-page");
+  historyFigure.classList.add("hidden", true);
+});
--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,8 @@
 document.getElementById("donate-1").addEventListener("click", function () {
   const addMoney = getInputFieldValueById("input-1");
 
+  document.getElementById("input-1").value = "";
+
   if (isNaN(addMoney)) {
     alert("Please input a number");
     return;
@@ -22,11 +24,27 @@ document.getElementById("donate-1").addEventListener("click", function () {
   document.getElementById("bank-balance").innerText = updatedBankBalance;
 
   document.getElementById("total-1").innerText = updatedDeposit;
+
+  const noakhali = document.getElementById("noakhali").innerText;
+
+  const historyItem = document.createElement("div");
+  historyItem.className = "p-5 border border-secondary";
+  historyItem.innerHTML = `
+  
+  <h5 class="text-lg font-bold">${addMoney} TK is donated for ${noakhali}</h5>
+  <p class="text-para text-sm">${new Date().toLocaleDateString()}</p>
+  
+  `;
+  const hisotyFigure = document.getElementById("history-page");
+
+  hisotyFigure.insertBefore(historyItem, hisotyFigure.firstChild);
 });
 
 // card-2
 document.getElementById("donate-2").addEventListener("click", function () {
   const addMoney2 = getInputFieldValueById("input-2");
+
+  document.getElementById("input-2").value = "";
 
   if (isNaN(addMoney2)) {
     alert("Please input a number");
@@ -48,30 +66,60 @@ document.getElementById("donate-2").addEventListener("click", function () {
   document.getElementById("bank-balance").innerText = updatedBankBalance2;
 
   document.getElementById("total-2").innerText = updatedDeposit2;
+
+  const feni = document.getElementById("feni").innerText;
+
+  const historyItem = document.createElement("div");
+  historyItem.className = "p-5 border border-secondary";
+  historyItem.innerHTML = `
+  
+  <h5 class="text-lg font-bold">${addMoney2} TK is donated for ${feni}</h5>
+  <p class="text-para text-sm">${new Date().toLocaleDateString()}</p>
+  
+  `;
+  const hisotyFigure = document.getElementById("history-page");
+
+  hisotyFigure.insertBefore(historyItem, hisotyFigure.firstChild);
 });
 
 // card-3
 document.getElementById("donate-3").addEventListener("click", function () {
-  const addMoney = getInputFieldValueById("input-3");
+  const addMoney3 = getInputFieldValueById("input-3");
 
-  if (isNaN(addMoney)) {
+  document.getElementById("input-3").value = "";
+
+  if (isNaN(addMoney3)) {
     alert("Please input a number");
     return;
   }
 
-  const deposit = getTotalValueById("total-3");
-  const updatedDeposit = deposit + addMoney;
+  const deposit3 = getTotalValueById("total-3");
+  const updatedDeposit3 = deposit3 + addMoney3;
 
-  const bankBalance = getTotalValueById("bank-balance");
+  const bankBalance3 = getTotalValueById("bank-balance");
 
-  if (addMoney > bankBalance) {
+  if (addMoney3 > bankBalance3) {
     alert("You don't have enough money");
     return;
   }
 
-  const updatedBankBalance = bankBalance - addMoney;
+  const updatedBankBalance3 = bankBalance3 - addMoney3;
 
-  document.getElementById("bank-balance").innerText = updatedBankBalance;
+  document.getElementById("bank-balance").innerText = updatedBankBalance3;
 
-  document.getElementById("total-3").innerText = updatedDeposit;
+  document.getElementById("total-3").innerText = updatedDeposit3;
+
+  const movement = document.getElementById("movement").innerText;
+
+  const historyItem = document.createElement("div");
+  historyItem.className = "p-5 border border-secondary";
+  historyItem.innerHTML = `
+  
+  <h5 class="text-lg font-bold">${addMoney3} TK is donated for ${movement}</h5>
+  <p class="text-para text-sm">${new Date().toLocaleDateString()}</p>
+  
+  `;
+  const hisotyFigure = document.getElementById("history-page");
+
+  hisotyFigure.insertBefore(historyItem, hisotyFigure.firstChild);
 });
